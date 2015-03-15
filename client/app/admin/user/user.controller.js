5f6de506a3579d31bced3ea79bec33d3e4bc3190
Fabio Lemos Elizandro <fabio@elizandro.com.br>
2015-03-15 14:20:17 -0300

    Exibindo erros mongoose

--- client/app/admin/user/user.controller.js
+++ client/app/admin/user/user.controller.js
@@ -69,7 +69,6 @@ angular.module('tapApp')
         err = err.data;
         $scope.errors = {};
 
-        // Update validity of form fields that match the mongoose errors
         angular.forEach(err.errors, function(error, field) {
           form[field].$setValidity('mongoose', false);
           $scope.errors[field] = error.message;
